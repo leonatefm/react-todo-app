46a6b1b5e6199ec7b0ed7cdcb1b30eb1073de62b
Chengqi Zhu <chengqizhufm@gmail.com>
2018-06-21 22:52:22 -0500

    Added immutability-helper to improve render performance

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import '../css/app.css';
 import data from '../data/data.json';
+import update from 'immutability-helper';
 
 const colorMap = {
 	"red": "#ff3b30",
@@ -15,7 +16,7 @@ const colorMap = {
 };
 
 class TodoApp extends Component {
-	constructor(props){
+	constructor(props) {
 		super(props);
 		this.state = {
 			todoLists: data,
@@ -28,27 +29,27 @@ class TodoApp extends Component {
 		this.searchList = this.searchList.bind(this);
 	}
 
-	switchList(listId){
+	switchList(listId) {
 		this.setState({
 			currentListId: listId,
 			searchKeyword: ""
 		});
 	}
 
-	updateList(action, listId, list){
+	updateList(action, listId, list) {
 		const todoLists = this.state.todoLists;
 		const index = todoLists.findIndex(item => item.listId === listId);
 		let newCurrentListId = this.state.currentListId;
 
-		if(action === "update"){
+		if (action === "update") {
 			todoLists[index] = list;
-		}else if(action === "delete" && todoLists.length>1){
-			todoLists.splice(index, 1);	
+		} else if (action === "delete" && todoLists.length > 1) {
+			todoLists.splice(index, 1);
 			//Reset current list to the first list when it gets deleted
-			if(this.state.currentListId === listId){
+			if (this.state.currentListId === listId) {
 				newCurrentListId = todoLists[0].listId;
 			}
-		}else if(action === "add"){
+		} else if (action === "add") {
 			todoLists.push(list);
 			newCurrentListId = listId;
 		}
@@ -59,13 +60,13 @@ class TodoApp extends Component {
 		});
 	}
 
-	searchList(keyword){
-		if(keyword.length>0){
+	searchList(keyword) {
+		if (keyword.length > 0) {
 			this.setState({
 				currentListId: "",
 				searchKeyword: keyword
 			});
-		}else{
+		} else {
 			this.setState({
 				currentListId: this.state.todoLists[0].listId,
 				searchKeyword: ""
@@ -78,20 +79,20 @@ class TodoApp extends Component {
 		let currentList;
 		const searchKeyword = this.state.searchKeyword;
 
-		if(searchKeyword && searchKeyword.length>0){
+		if (searchKeyword && searchKeyword.length > 0) {
 			let searchResults = [];
-			this.state.todoLists.forEach(function(list, index){
+			this.state.todoLists.forEach(function (list, index) {
 				let listItem = [];
-				list.items.forEach(function(item){
-					if(item.title.toLowerCase().includes(searchKeyword.toLowerCase())) listItem.push(item);
+				list.items.forEach(function (item) {
+					if (item.title.toLowerCase().includes(searchKeyword.toLowerCase())) listItem.push(item);
 				})
-				if(listItem.length>0){
-					searchResults.push({sectionId: index, name: list.name, color: list.color, items: listItem});
+				if (listItem.length > 0) {
+					searchResults.push({ listId: list.listId, name: list.name, color: list.color, items: listItem });
 				}
 			});
-			currentList = {name: 'Results for "'+ searchKeyword +'"', items:searchResults, isSearchResults: true};
-		}else{
-			currentList = this.state.todoLists.find(list => list.listId === this.state.currentListId);  
+			currentList = { name: 'Results for "' + searchKeyword + '"', items: searchResults, isSearchResults: true };
+		} else {
+			currentList = this.state.todoLists.find(list => list.listId === this.state.currentListId);
 		}
 
 		return (
@@ -105,7 +106,7 @@ class TodoApp extends Component {
 
 class SideMenu extends Component {
 
-	constructor(props){
+	constructor(props) {
 		super(props);
 		this.state = {
 			mode: 'view'
@@ -115,7 +116,7 @@ class SideMenu extends Component {
 		this.addList = this.addList.bind(this);
 	}
 
-	editLists(){
+	editLists() {
 		this.setState({
 			mode: this.state.mode === "view" ? "edit" : "view"
 		});
@@ -126,7 +127,7 @@ class SideMenu extends Component {
 		const newList = {
 			listId: (Date.now() + Math.random()).toString(36).substr(2),
 			name: "New List",
-			color: colors[Math.floor(Math.random()*colors.length)],
+			color: colors[Math.floor(Math.random() * colors.length)],
 			items: [],
 			isNew: true
 		};
@@ -144,7 +145,7 @@ class SideMenu extends Component {
 		return (
 			<div className={"side-menu " + this.state.mode + "-mode"}>
 				<h3>Reactodo</h3>
-				<div className="search-box"><input type="text" placeholder="Search" value={this.props.searchKeyword} onChange={(e)=>{this.props.searchList(e.target.value)}}/></div>
+				<div className="search-box"><input type="text" placeholder="Search" value={this.props.searchKeyword} onChange={(e) => { this.props.searchList(e.target.value) }} /></div>
 				<ul className="todo-lists">
 					{todoListElems}
 				</ul>
@@ -157,126 +158,86 @@ class SideMenu extends Component {
 	}
 };
 
-class ListItem extends Component {
+class ListItem extends React.PureComponent {
 
-	constructor(props){
+	constructor(props) {
 		super(props);
 		this.textInput = React.createRef();
 		this.handleListChange = this.handleListChange.bind(this);
 		this.handleListOnBlur = this.handleListOnBlur.bind(this);
 	}
 
-	componentDidMount(){
+	componentDidMount() {
 		//Autofocus on new todo item
-		if(this.props.list.isNew){
+		if (this.props.list.isNew) {
 			this.textInput.current.focus();
 			this.textInput.current.select();
 		}
 	}
 
-	handleListChange(e){
+	handleListChange(e) {
 		const list = this.props.list;
+		let newList;
 
-		if(e.type === "change"){
-			list.name = e.target.value;
-			this.props.updateList('update', list.listId, list);
-		}else if(e.type === "click"){
+		if (e.type === "change") {
+			newList = update(list, { name: { $set: e.target.value } });
+			this.props.updateList('update', list.listId, newList);
+		} else if (e.type === "click") {
 			this.props.updateList('delete', list.listId);
 			e.stopPropagation();
 		}
 	}
 
-	handleListOnBlur(e){
-		
+	handleListOnBlur(e) {
+
 		const list = this.props.list;
 
-		if(!list.isNew) return;
+		if (!list.isNew) return;
 
-		if(list.name.length === 0){
+		if (list.name.length === 0) {
 			this.props.updateList("delete", list.listId);
-		}else{
+		} else {
 			delete list.isNew;
 			this.props.updateList("update", list.listId, list);
 		}
 	}
 
 	render() {
-		
+
 		const list = this.props.list;
 		const listClass = this.props.isActive ? 'active' : '';
 
 		return (
-			<li className={listClass} onClick={()=>{if(this.props.mode==='edit') return; this.props.switchList(list.listId)}}>
-				<input className="list-name" ref={this.textInput} type="text" value={list.name} disabled={(this.props.mode === "view" && !this.props.list.isNew ) ? true : false} onChange={this.handleListChange} onBlur={this.handleListOnBlur}/>
+			<li className={listClass} onClick={() => { if (this.props.mode === 'edit') return; this.props.switchList(list.listId) }}>
+				<input className="list-name" ref={this.textInput} type="text" value={list.name} disabled={(this.props.mode === "view" && !this.props.list.isNew) ? true : false} onChange={this.handleListChange} onBlur={this.handleListOnBlur} />
 				<span className="list-number">{this.props.mode === "view" ? list.items.length : <ion-icon name="close" onClick={this.handleListChange}></ion-icon>}</span>
 			</li>
 		);
 	}
 };
 
-class ListDetails extends Component {
+class ListDetails extends React.PureComponent {
 
-	constructor(props){
+	constructor(props) {
 		super(props);
-
-		this.editItem = this.editItem.bind(this);
-		this.addItem = this.addItem.bind(this);
-	}
-
-	editItem(action, itemId, item){
-		const list = this.props.list;
-		const index = this.props.list.items.findIndex(item => item.itemId === itemId);
-
-		if(action === "update"){
-			list.items[index] = item;
-		}else if(action === "delete"){
-			list.items.splice(index, 1);	
-		}
-
-		this.props.updateList('update', list.listId, list);
-	}
-
-	addItem(){
-		const newItem = {
-			itemId: (Date.now() + Math.random()).toString(36).substr(2),
-			title: "",
-			isComplete: false,
-			isNew: true
-		};
-		const list = this.props.list;
-
-		list.items.push(newItem);
-
-		this.props.updateList('update', list.listId, list);
 	}
 
 	render() {
-		
+
 		const list = this.props.list;
 		let listContent;
 
 		//Generate list content based on context
-		if(!list.isSearchResults){
-			const todoItems = list.items.map((item) => <TodoItem key={item.itemId} item={item} editItem={this.editItem} color={list.color}></TodoItem>)
-			listContent = <ul className="list-items">{todoItems}</ul>;
-		}else{
-			listContent = list.items.map((section)=>{
-				const sectionItems = section.items.map((item) => <TodoItem key={item.itemId} item={item} editItem={this.editItem} color={section.color}></TodoItem>);
-				const titleStyle = {
-					color: colorMap[section.color] ? colorMap[section.color] : "#000000" 
-				}
-				return (
-					<section key={section.sectionId} className="list-section">
-						<h5 style={titleStyle}>{section.name}</h5>
-						<ul className="list-items">{sectionItems}</ul>
-					</section>
-				);
+		if (!list.isSearchResults) {
+			listContent = <ListSection list={list} updateList={this.props.updateList}></ListSection>
+		} else {
+			listContent = list.items.map((section) => {
+				return <ListSection key={section.listId} list={section} updateList={this.props.updateList} sectionTitle="true"></ListSection>
 			});
 		}
 
-
 		const titleStyle = {
-			color: colorMap[list.color] ? colorMap[list.color] : "#000000" 
+			color: colorMap[list.color] ? colorMap[list.color] : "#000000"
 		}
 
 		return (
@@ -293,9 +254,64 @@ class ListDetails extends Component {
 	}
 };
 
-class TodoItem extends Component {
+class ListSection extends React.PureComponent {
+	constructor(props) {
+		super(props);
 
-	constructor(props){
+		this.editItem = this.editItem.bind(this);
+		this.addItem = this.addItem.bind(this);
+	}
+
+	editItem(action, itemId, item) {
+		const list = this.props.list;
+		const index = this.props.list.items.findIndex(item => item.itemId === itemId);
+		let newList;
+
+		if (action === "update") {
+			newList = update(list, { items: { [index]: { $set: item } } });
+		} else if (action === "delete") {
+			newList = update(list, { items: { $splice: [[index, 1]] } });
+		}
+
+		this.props.updateList('update', list.listId, newList);
+	}
+
+	addItem() {
+		const newItem = {
+			itemId: (Date.now() + Math.random()).toString(36).substr(2),
+			title: "",
+			isComplete: false,
+			isNew: true
+		};
+		const list = this.props.list;
+
+		let newList = update(list, { items: { $push: [newItem] } })
+
+		this.props.updateList('update', list.listId, newList);
+	}
+
+	render() {
+
+		const list = this.props.list;
+
+		const todoItems = list.items.map((item) => <TodoItem key={item.itemId} item={item} editItem={this.editItem} color={list.color}></TodoItem>);
+
+		const titleStyle = {
+			color: colorMap[list.color] ? colorMap[list.color] : "#000000"
+		}
+
+		return (
+			<section className="list-section">
+				{this.props.sectionTitle && <h5 style={titleStyle}>{list.name}</h5>}
+				<ul className="list-items">{todoItems}</ul>
+			</section>
+		)
+	}
+}
+
+class TodoItem extends React.PureComponent {
+
+	constructor(props) {
 		super(props);
 		this.textInput = React.createRef();
 		this.handleItemChange = this.handleItemChange.bind(this);
@@ -303,41 +319,44 @@ class TodoItem extends Component {
 		this.handleItemOnBlur = this.handleItemOnBlur.bind(this);
 	}
 
-	componentDidMount(){
+	componentDidMount() {
 		//Autofocus on new todo item
-		if(this.props.item.isNew){
+		if (this.props.item.isNew) {
 			this.textInput.current.focus();
 		}
 	}
 
-	handleItemOnBlur(e){
-		
+	handleItemOnBlur(e) {
+
 		const item = this.props.item;
 
-		if(!item.isNew) return;
+		if (!item.isNew) return;
 
-		if(item.title.length === 0){
+		let newItem;
+
+		if (item.title.length === 0) {
 			this.props.editItem("delete", item.itemId);
-		}else{
-			delete item.isNew;
-			this.props.editItem("update", item.itemId, item);
+		} else {
+			newItem = update(item, { $unset: ["isNew"] });
+			this.props.editItem("update", item.itemId, newItem);
 		}
 	}
 
-	handleItemChange(e){
-	
+	handleItemChange(e) {
+
 		const item = this.props.item;
+		let newItem;
 
-		if(e.type === "change"){
-			item.title = e.target.value;
-		}else if(e.type === "click"){
-			item.isComplete = !item.isComplete;
+		if (e.type === "change") {
+			newItem = update(item, { title: { $set: e.target.value } });
+		} else if (e.type === "click") {
+			newItem = update(item, { isComplete: { $apply: function (value) { return !value } } });
 		}
 
-		this.props.editItem("update", item.itemId, item);
+		this.props.editItem("update", item.itemId, newItem);
 	}
 
-	handleItemDelete(e){
+	handleItemDelete(e) {
 
 		const item = this.props.item;
 
@@ -348,16 +367,16 @@ class TodoItem extends Component {
 
 		const status = this.props.item.isComplete ? "complete" : "pending";
 		const bgStyle = this.props.item.isComplete ? {
-			backgroundColor: colorMap[this.props.color] ? colorMap[this.props.color] : "#B5B5B5" 
+			backgroundColor: colorMap[this.props.color] ? colorMap[this.props.color] : "#B5B5B5"
 		} : {};
 
 		return (
 			<li className={status}>
 				<span className="todo-status" onClick={this.handleItemChange}><span className="fill" style={bgStyle}></span></span>
-				<input className="todo-content" ref={this.textInput} type="text" value={this.props.item.title} onChange={this.handleItemChange} onBlur={this.handleItemOnBlur}/>
+				<input className="todo-content" ref={this.textInput} type="text" value={this.props.item.title} onChange={this.handleItemChange} onBlur={this.handleItemOnBlur} />
 				<a className="delete-btn" onClick={this.handleItemDelete}><ion-icon name="close"></ion-icon></a>
 			</li>
-		)	
+		)
 	}
 };
 
